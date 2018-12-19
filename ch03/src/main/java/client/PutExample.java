99515c3de5adc314f703c92b6b1d788d232d0adb
vonzhou <vonzhou@163.com>
2018-12-19 10:44:53 +0800

    Put

--- ch03/src/main/java/client/PutExample.java
+++ ch03/src/main/java/client/PutExample.java
@@ -2,6 +2,7 @@ package client;
 
 // cc PutExample Example application inserting data into HBase
 // vv PutExample
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
@@ -16,32 +17,40 @@ import util.HBaseHelper;
 
 import java.io.IOException;
 
+
+/**
+ * ex3.2
+ */
 public class PutExample {
 
-  public static void main(String[] args) throws IOException {
-    Configuration conf = HBaseConfiguration.create(); // co PutExample-1-CreateConf Create the required configuration.
+    public static void main(String[] args) throws IOException {
+        Configuration conf = HBaseConfiguration.create(); // co PutExample-1-CreateConf Create the required configuration.
 
-    // ^^ PutExample
-    HBaseHelper helper = HBaseHelper.getHelper(conf);
-    helper.dropTable("testtable");
-    helper.createTable("testtable", "colfam1");
-    // vv PutExample
-    Connection connection = ConnectionFactory.createConnection(conf);
-    Table table = connection.getTable(TableName.valueOf("testtable")); // co PutExample-2-NewTable Instantiate a new client.
+        // ^^ PutExample
+        HBaseHelper helper = HBaseHelper.getHelper(conf);
+        helper.dropTable("testtable");
+        helper.createTable("testtable", "colfam1");
+        // vv PutExample
+        Connection connection = ConnectionFactory.createConnection(conf);
+        Table table = connection.getTable(TableName.valueOf("testtable")); // co PutExample-2-NewTable Instantiate a new client.
 
-    Put put = new Put(Bytes.toBytes("row1")); // co PutExample-3-NewPut Create put with specific row.
+        Put put = new Put(Bytes.toBytes("row1")); // co PutExample-3-NewPut Create put with specific row.
 
-    put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"),
-      Bytes.toBytes("val1")); // co PutExample-4-AddCol1 Add a column, whose name is "colfam1:qual1", to the put.
-    put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"),
-      Bytes.toBytes("val2")); // co PutExample-4-AddCol2 Add another column, whose name is "colfam1:qual2", to the put.
+        put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual1"),
+                Bytes.toBytes("val1")); // co PutExample-4-AddCol1 Add a column, whose name is "colfam1:qual1", to the put.
+        put.addColumn(Bytes.toBytes("colfam1"), Bytes.toBytes("qual2"),
+                Bytes.toBytes("val2")); // co PutExample-4-AddCol2 Add another column, whose name is "colfam1:qual2", to the put.
 
-    table.put(put); // co PutExample-5-DoPut Store row with column into the HBase table.
-    table.close(); // co PutExample-6-DoPut Close table and connection instances to free resources.
-    connection.close();
-    // ^^ PutExample
-    helper.close();
-    // vv PutExample
-  }
+
+        System.out.println("本次会添加的KeyValue实例数量 : " + put.size());
+        System.out.println();
+
+        table.put(put); // co PutExample-5-DoPut Store row with column into the HBase table.
+        table.close(); // co PutExample-6-DoPut Close table and connection instances to free resources.
+        connection.close();
+        // ^^ PutExample
+        helper.close();
+        // vv PutExample
+    }
 }
 // ^^ PutExample
